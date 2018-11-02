334bdf72b59467b77aebaf65b24ff3e44bf618ae
Felipe Teixeira Moraes <felipetm6@hotmail.com>
2018-11-02 15:47:59 -0300

    Adicionar exercício de empréstimo

---
+++ exercicio-emprestimo.js
@@ -0,0 +1,14 @@
+/*
+Você trabalha em uma financeira. É necessário escrever um programa que avalie um empréstimo. As entradas do programa são: a renda do cliente, a idade do cliente e o valor que ele deseja emprestar.
+Regras para aprovar um empréstimo:
+- Emprestamos para pessoas que tenham entre 25 e 65 anos;
+- Renda mínima para empréstimo é de R$1500,00 ou mais;
+- O valor do empréstimo deve estar entre R$1000,00 e 10 vezes a renda declarada;
+Seu programa deve responder "APROVADO!" ou "NÃO APROVADO!" de acordo com cliente;
+Bônus: Se o empréstimo for aprovado, perguntamos em quantas parcelas ele quer pagar. 
+Regras para parcela:
+- Parcelamos entre 5 e 24 meses;
+- Mostrar o valor da parcela (com taxa de juro composto de 8%) e a quantidade de parcelas para pagamento;
+- Se o valor de parcelas for inválido e o valor for menor que 5, o empréstimo é feito em 5 parcelas;
+- Se o valor de parcelas for inválido e o valor for maior que 24, o empréstimo é feito em 24 parcelas;
+*/
